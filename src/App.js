9674aae9e0d87054af232fe691d747b2f2297881
suraj <wavsuraj@gmail.com>
2020-08-11 14:31:01 +0530

    Temp Changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+//import './App.css';
+import Autocomplete from "./Autocomplete";
+const App = () => {
+  return (
+    <div className="App">
+      <Autocomplete
+        options={[
+          "Papaya",
+          "Persimmon",
+          "Paw Paw",
+          "Prickly Pear",
+          "Peach",
+          "Pomegranate",
+          "Pineapple"
+        ]}
+      />
+    </div>
+  );
+};
+
+export default App;
